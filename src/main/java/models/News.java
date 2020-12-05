4bd8428f04e13b526ee06f8eb22a5c68a177a155
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-12-05 14:47:03 +0200

    Added variable to the News class, it's constructor, setters, getters and equals method

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,107 @@
 package models;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class News {
+    public int id;
+    public String tittle;
+    public String writtenBy;
+    public String content;
+    public String type;
+    public int employee_id;
+    public int department_id;
+    private long createdat;
+    private String formattedCreatedAt;
+
+    private final String TYPE_OF_NEWS="general";
+
+    public News(String name,String tittle,String writtenBy, String content,int employee_id,int department_id) {
+        this.writtenBy = writtenBy;
+        this.tittle = tittle;
+        this.content = content;
+        this.type = TYPE_OF_NEWS;
+        this.employee_id = employee_id;
+        this.department_id=0;
+        this.createdat = System.currentTimeMillis();
+        setFormattedCreatedAt();
+
+    }
+
+
+    public String getTittle() {
+        return tittle;
+    }
+    public String getWrittenBy() {
+        return writtenBy;
+    }
+    public String getContent() {
+        return content;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+    public int getEmployee_id() {
+        return employee_id;
+    }
+    public int getDepartment_id() {
+        return department_id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public long getCreatedat() {
+        return createdat;
+    }
+
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+    public void setWrittenBy(String writtenBy) {
+        this.writtenBy = writtenBy;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setCreatedat(long createdat) {
+        this.createdat = createdat;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setEmployee_id(int employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public void setDepartment_id(int department_id) {
+        this.department_id = department_id;
+    }
+
+
+    public String getFormattedCreatedAt(){
+        Date date = new Date(createdat);
+        String datePatternToUse = "MM/dd/yyyy @ K:mm a"; //see https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
+        SimpleDateFormat sdf = new SimpleDateFormat(datePatternToUse);
+        return sdf.format(date);
+    }
+
+    public void setFormattedCreatedAt(){
+        Date date = new Date(this.createdat);
+        String datePatternToUse = "MM/dd/yyyy @ K:mm a";
+        SimpleDateFormat sdf = new SimpleDateFormat(datePatternToUse);
+        this.formattedCreatedAt = sdf.format(date);
+    }
+
 }
