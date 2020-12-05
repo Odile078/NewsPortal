26b0a62ac90c096cbaf223d7204fa0e3265f1ed1
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-12-05 14:05:23 +0200

    Added variable to the Department class, it's constructor, setters, getters and equals method

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,4 +1,44 @@
 package models;
 
+import java.util.Objects;
+
 public class Department {
+    private int id;
+    private String name;
+    private String description;
+
+    public Department(String name,String description) {
+        this.description = description;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return id == that.id &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description);
+    }
 }
