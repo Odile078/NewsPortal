4c5289ea2b20faf149e5cf74fcb5b025562cdd31
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-12-05 13:58:13 +0200

    Added variable to the Employee class, it's constructor, setters, getters and equals method

--- src/main/java/models/Employee.java
+++ src/main/java/models/Employee.java
@@ -1,4 +1,57 @@
 package models;
 
+import java.util.Objects;
+
 public class Employee {
+    private int id;
+    private String name;
+    private String position;
+    private String role;
+
+    public Employee(String name, String position, String role){
+        this.name = name;
+        this.position = position;
+        this.position = role;
+        this.id =id;
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id &&
+                Objects.equals(name, employee.name) &&
+                Objects.equals(position, employee.position) &&
+                Objects.equals(role, employee.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, position, role);
+    }
 }
